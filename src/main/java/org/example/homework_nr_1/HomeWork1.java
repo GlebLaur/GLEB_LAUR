50124ae089ce16d681dfcf3e6d4d315359a0dfc0
GlebLaur <young.gleb@gmail.com>
2023-03-21 22:06:12 +0200

    Homework: Variables in Java

---
+++ src/main/java/org/example/homework_nr_1/HomeWork1.java
@@ -0,0 +1,28 @@
+package org.example.homework_nr_1;
+
+public class HomeWork1 {
+    public static void main(String[] args){
+
+        System.out.println("Hello World!");
+
+        long currentWorldPopulation = 7811080815L;
+        int carsProducedThisYear = 55235358;
+        char firstLetterInAlphabet = 'A';
+        boolean javaIsAmazing = true;
+        float pi = 3.14159F;
+
+        System.out.println("Current World Population: " + currentWorldPopulation);
+        System.out.println("Cars Produced ThisYear: " + carsProducedThisYear);
+        System.out.println("First Letter In Alphabet: " + firstLetterInAlphabet);
+        System.out.println("Java Is Amazing: " + javaIsAmazing);
+        System.out.println("P = "+ pi);
+
+        double price = 233000.5F;
+        double tax = 20.5F;
+        int quantity = 13;
+        double sum = 0;
+        double total = price + tax * quantity + sum;
+        System.out.println("Total: " + total);
+
+    }
+}
